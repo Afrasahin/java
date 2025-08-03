46a9e0fb94e05b2e029e0f70b776ca7a0868e439
Afrasahin <Afrasahin11015@gmail.com>
2025-08-03 17:09:15 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,25 @@
+class first{
+	public static void main(String args[]){
+	//afrasahin
+	System.out.println("new name");
+	/*do not care about this*/
+	System.out.println("focus");
+	int a=11,b=7;
+	System.out.println(a+b);
+	System.out.println(a-b);
+	System.out.println(a*b);
+	System.out.println(a%b);
+	byte d=5,c=2;
+	System.out.println(d+c);
+	System.out.println(d-c);
+	System.out.println(d*c);
+	System.out.println(d%c);
+	float e=11.5f,g=12.4f;
+	System.out.println(e+g);
+	System.out.println(e-g);
+	System.out.println(e*g);
+	System.out.println(e%g);
+}
+}
+ 
+
